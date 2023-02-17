da5a2f0d5742451b0058f3a9e40b79013448974a
Tife75 <ogundelebolu75@gmail.com>
2023-02-16 18:15:39 -0800

    simple C program that print size of data types on machine it is run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu\n", sizeof(char));
-	printf("Size of int: %lu\n", sizeof(int));
-	printf("Size of long int: %lu\n", sizeof(long int));
-	printf("Size of long long int: %lu\n", sizeof(long long int));
-	printf("Size of float: %lu\n", sizeof(float));
+	printf("Size of char: %ld\n", sizeof(char));
+	printf("Size of int: %ld\n", sizeof(int));
+	printf("Size of long int: %ld\n", sizeof(long int));
+	printf("Size of long long int: %ld\n", sizeof(long long int));
+	printf("Size of float: %ld\n", sizeof(float));
 }
