9532b81213b6a7c5ecd78e660cea2db4f4eb570e
Tife75 <ogundelebolu75@gmail.com>
2023-02-16 16:09:17 -0800

    simple C program that prints to stdout using 'put'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
  * main - My program starts from here
  *
  * Return: 0 if the program runs without error
+ *
  */
 int main(void)
 {
-	char str[] = "\"programming is like building a multilingual puzzle";
-
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
