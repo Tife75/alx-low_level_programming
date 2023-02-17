1641be9980a31cc39c4e50029ca426af5ce97dfd
Tife75 <ogundelebolu75@gmail.com>
2023-02-16 16:20:43 -0800

    simple C program that prints to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is where my program starts from
+ *
+ * Return: 0 if program runs without error
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
