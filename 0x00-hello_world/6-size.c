b46fe438c3c13d10a18fc0c3ecb1762aa779fc76
Tife75 <ogundelebolu75>
2023-02-17 10:17:39 +0000

    simple C program that print size of data types on machine it is run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main- The main fuction where the program starts
- *
- * Return: 0 if program runs without error
- */
+  * main- This is where my program start
+  *
+  * Return: 0 if program runs without error
+  */
 int main(void)
 {
-	printf("Size of char: %li bytes(s)\n", sizeof(char));
-	printf("Size of int: %li bytes(s)\n", sizeof(int));
-	printf("Size of long int: %li bytes(s)\n", sizeof(long int));
-	printf("Size of long long int: %li bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %li bytes(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
