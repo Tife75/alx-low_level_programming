5bc23ea6667b98d6d243c8d13b9e95bde5d3880e
Tife75 <ogundelebolu75@gmail.com>
2023-02-18 03:49:07 -0800

    program to print the last digit of an unkonown number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,21 +10,20 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
-	else if ((n % 10) == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
-	else if ((n % 10) < 6)
-	{
-		printf("Last digit of %d is %d and is less than 6 amd not 0\n", n, n % 10);
-	}
+	lastDigit = n % 10;
+
+	printf("Last digit of %i is %i and is ", n, lastDigit);
+
+	if (lastDigit > 5)
+		printf("greater than 5\n");
+	else if (lastDigit == 0)
+		printf("0\n");
+	else
+		printf("less than 6 and not 0\n");
 	return (0);
 }
