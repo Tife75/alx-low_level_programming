a6ea44282f0207b369afb4ead89a152b569675d0
Tife75 <ogundelebolu75@gmail.com>
2023-02-18 04:52:39 -0800

    program that prints the complete alphabet using only two putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- This program prints the alphabet in lowercase
+ *
+ * Return: 0 if it runs without error
+ */
+int main(void)
+{
+	char alpha1 = 'a';
+	char alpha2 = 'z';
+
+	while (alpha1 <= alpha2)
+	{
+		putchar(alpha1);
+		alpha1++;
+	}
+	putchar('\n');
+}
