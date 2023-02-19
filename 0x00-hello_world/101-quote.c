b682cbe6fb4ddfd6bfa2ad758baf5ecaf8a197ca
Tife75 <ogundelebolu75@gmail.com>
2023-02-19 10:21:46 -0800

    using write function in a program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main- program to print using write function
  *
- * Return: 0 if no error
+ * Return: 1 if no error
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
