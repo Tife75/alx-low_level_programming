b2a4a5835bfd1e6e61836d6b26e92cb9b51eccdc
Tife75 <ogundelebolu75>
2023-02-21 22:53:55 +0000

    10 times over

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This program prints the alphabet in lowercase x10
+ *
+ * Return: Null if no error
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		
+	}
+}
