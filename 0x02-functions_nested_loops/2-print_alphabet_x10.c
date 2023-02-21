599c2254d6fe6467d0850fece0aba5813e924a0b
Tife75 <ogundelebolu75@gmail.com>
2023-02-21 15:17:12 -0800

    10 x alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char alpha = 'a';
+	char alpha;
+	int i;
 
-	while (alpha <= 'z')
+	for (i = 0; i < 10; i++)
 	{
-		
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	_putchar('\n');
 	}
 }
