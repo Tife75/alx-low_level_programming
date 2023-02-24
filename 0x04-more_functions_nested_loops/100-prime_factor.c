1b560165cae6db1a4ec46a623e356eb70b934254
Tife75 <ogundelebolu75@gmail.com>
2023-02-24 06:39:30 -0800

    program to print prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - program to print prime factors
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	long num = 612852475143;
+
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+
+	return (0);
+}
