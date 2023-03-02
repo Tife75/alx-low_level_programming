8a4f8ee18195f054ad8b642cb1f88a2323eeb944
Tife75 <ogundelebolu75>
2023-03-02 16:44:31 +0000

    function to cocatenate string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,19 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
-		*(dest + i) = src[j];
-		j++;
-		i++;
+		dest[i] = src[j];
 	}
 
-	*(dest + 1) = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
