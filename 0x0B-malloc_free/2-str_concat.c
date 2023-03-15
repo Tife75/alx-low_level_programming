14a595a76ea33e20eb2f8136c0a7451c205ad158
Tife75 <ogundelebolu75@gmail.com>
2023-03-15 11:55:58 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,48 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * alloc_grid - function that concatenates two strings.
- * @width: int size width
- * @height: int size height
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Return: Pointer to new grid
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-
-int **alloc_grid(int width, int height)
+char *str_concat(char *s1, char *s2)
 {
-	int **grid;
-	int i, j;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (width + height < 2 || width < 1 || height < 1)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
 
-	grid = malloc(height * sizeof(*grid));
-	if (grid == NULL)
-		return (NULL);
+	i = 0;
+	j = 0;
 
-	for (i = 0; i < height; i++)
+	if (s1)
 	{
-		grid[i] = malloc(width * sizeof(**grid));
-		if (grid[i] == NULL)
+		while (i < len1)
 		{
-			for (i--; i >= 0; i--)
-				free(grid[i]);
-			free(grid);
-			return (NULL);
+			s3[i] = s1[i];
+			i++;
 		}
-		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
 	}
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 
-	return (grid);
+	return (s3);
 }
